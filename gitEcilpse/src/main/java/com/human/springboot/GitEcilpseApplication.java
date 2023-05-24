7b932c1fe08b74354c508e5117233213d49d15bd
koreaBEAR <128657240+koreaBEAR@users.noreply.github.com>
2023-05-24 12:15:10 +0900

    Test1

---
+++ gitEcilpse/src/main/java/com/human/springboot/GitEcilpseApplication.java
@@ -0,0 +1,13 @@
+package com.human.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitEcilpseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitEcilpseApplication.class, args);
+	}
+
+}
